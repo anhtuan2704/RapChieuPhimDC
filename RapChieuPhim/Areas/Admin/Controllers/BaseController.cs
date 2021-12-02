8d84cf565b0bd15d48285a3897b78f9af2da37ab
anhtuan2704 <anhtuan2704222@gmail,com>
2021-12-02 23:27:31 +0700

    add file

---
+++ RapChieuPhim/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using RapChieuPhim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapChieuPhim.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+                base.OnActionExecuted(filterContext);
+        }
+    }
+}
---
+++ RapChieuPhim/Controllers/TaiKhoanController.cs
@@ -0,0 +1,58 @@
+﻿using RapChieuPhim.Dao;
+using RapChieuPhim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RapChieuPhim.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        // GET: TaiKhoan
+       [HttpPost]
+        public bool Login(string username,string password)
+        {
+            var dao = new TaiKhoanDao();
+            
+                TaiKhoan taiKhoan = dao.checkTaikhoan(username,password);
+            if (taiKhoan != null)
+            {
+                Session.Add("USERSESSIO",taiKhoan);
+                return true;
+
+            }
+            return false;
+        }
+        [HttpPost]
+        public bool Register(string username,string email, string password)
+        {
+            Boolean check = true;
+          
+                var dao = new TaiKhoanDao();
+          
+            TaiKhoan taiKhoan = new TaiKhoan();
+
+                taiKhoan.TenDangNhap = email;
+                taiKhoan.MatKhau = password;
+                taiKhoan.TinhTrang = true;
+                taiKhoan.NgayDangKy = DateTime.Now;
+                dao.SaveTaiKhoan(taiKhoan);
+
+            
+          
+            return check;
+       
+        }
+        public ActionResult Logout(string username, string email, string password)
+        {
+
+            Session.RemoveAll();
+
+            return Redirect("/Home");
+
+        }
+
+    }
+}
